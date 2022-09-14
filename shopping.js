45a2457cafa258b5118b3752c6f02f7feb19d635
Masse04 <massamba084@gmail.com>
2022-09-15 00:57:52 +0100

    enregistrement 2

--- shopping.js
+++ shopping.js
@@ -1,25 +1,40 @@
 var plus = document.querySelectorAll(".plus")
 var moins = document.querySelectorAll(".moins")
 
-
-for(var i = 0; i < plus.length; i++){
-    plus[i].addEventListener("click", function(){
+for(var i = 0; i < plus.length; i++)
+{
+    plus[i].addEventListener
+    ("click", function()
+    {
         var par = this.parentNode
         var text = par.querySelector(".texte")
         var count = parseInt(text.value)
         count += 1
         text.value = count
-        
-    })
+        var total = par.parentNode
+        var unite = total.querySelector(".unite .prix")
+        var prix = parseInt(unite.textContent)
+        prix *= count
+        unite.textContent = prix   
+    }
+    )
 }
+
+
 for(var i = 0; i < plus.length; i++){
     moins[i].addEventListener("click", function(){
         var par = this.parentNode
         var text = par.querySelector(".texte")
         var count = parseInt(text.value)
         count -= 1
-        text.value = count
-        
+        text.value = count    
+        var total = par.parentNode
+        var unite = total.querySelector(".unite .prix")
+        var prix = parseInt(unite.textContent)
+        prix /= count
+        unite.textContent = prix
+
+
     })
 }
 
